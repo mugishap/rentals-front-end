e4c9da7533e755dd448f633aee4bae55cf26b54e
Mugisha Precieux <precieuxmugisha@gmail.com>
2022-01-19 18:25:39 +0200

    dark theme was added

---
+++ js/home.js
@@ -0,0 +1,7 @@
+
+function lightTheme(){
+document.body.style.backgroundColor = "white"
+}
+function darkTheme(){
+    document.body.style.background = "black"
+    }
--- js/signup.js
+++ js/signup.js
@@ -1,10 +1,11 @@
-let confirmation = document.getElementById("submit-btn")
-let pass1 = (document.getElementById("password-input")).value
-let pass2 = (document.getElementById("confirm-input")).value
-
-confirmation.addEventListener("click", () => {
-    if (pass1 != pass2){
-        alert("The password entered do not match!!")
-    }
-})
-
+function confpass(){
+    let confirmation = document.querySelector("#submit-btn")
+    let pass1 = (document.querySelector("#password-input"))
+    let pass2 = (document.querySelector("#confirm-input"))
+    
+    confirmation.addEventListener("click", () => {
+        if (pass1 != pass2){
+            window.alert("The passwords entered do not match!!")
+        }
+    })
+}
