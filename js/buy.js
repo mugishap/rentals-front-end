6e821d46027356e63596742b065fe0d74b7d4a83
Mugisha Precieux <precieuxmugisha@gmail.com>
2022-01-22 21:44:16 +0200

    edited some stuffs

--- js/buy.js
+++ js/buy.js
@@ -1,15 +1,17 @@
 
 function darkTheme(){
-    document.body.classList.toggle("darkTheme") = "black"
+    document.body.style.backgroundColor = "black"
     document.querySelector("#navbar").style.background = "darkblue"
-    if(document.body.contains(darkTheme)){
-        document.body.style.backgroundColor = "white"
-    }
-    else{
-        document.body.style.background = "black"
-        document.querySelector("#navbar").style.background = "darkblue"
+    a
+    document.querySelector("#goToDiv").style.cssText = ` = 
+    background:darkblue;
+    color:darkblue;
+    `
 
     }
+    function lightTheme(){
+        document.body.style.backgroundColor = "white"
+        document.querySelector("#navbar").style.background = "#3d5af1"
     }
     
 // const nav = document.querySelector('#navbar')
