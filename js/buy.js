df207b09afbb480304a091adbe8058b7e5b3f093
Mugisha Precieux <precieuxmugisha@gmail.com>
2022-01-22 16:09:36 +0200

    adding dark theme

--- js/buy.js
+++ js/buy.js
@@ -1,33 +1,29 @@
 
-function lightTheme(){
-document.body.style.backgroundColor = "white"
-}
 function darkTheme(){
-    document.body.style.background = "black"
+    document.body.classList.toggle("darkTheme") = "black"
     document.querySelector("#navbar").style.background = "darkblue"
-    document.querySelector("#searchBox").style.background = "barkblue"
-
-    }
-    
-const nav = document.querySelector('#navbar')
-window.addEventListener('scroll', fixNav)
-
-// function fixNav() {
-//     if(window.scrollY > nav.offsetHeight + 150) {
-//         nav.classList.add('active')
-//     } else {
-//         nav.classList.remove('active')
-//     }
-// }
-var icon = document.getElementById("modeChanger");
-
-icon.onclick = function darkMode() {
-    document.body.classList.toggle(darkTheme());
-    if(document.body.contains(darkTheme())){
-        icon.src="../images/icons/sun.png";
+    if(document.body.contains(darkTheme)){
+        document.body.style.backgroundColor = "white"
     }
     else{
-        icon.src= "../images/icons/moon.png";
-    }
+        document.body.style.background = "black"
+        document.querySelector("#navbar").style.background = "darkblue"
 
-}
+    }
+    }
+    
+// const nav = document.querySelector('#navbar')
+// window.addEventListener('scroll', fixNav)
+
+// // function fixNav() {
+// //     if(window.scrollY > nav.offsetHeight + 150) {
+// //         nav.classList.add('active')
+// //     } else {
+// //         nav.classList.remove('active')
+// //     }
+// // }
+// var icon = document.getElementById("modeChanger");
+
+// icon.onclick = function darkMode() {
+//     document.body.classList.toggle(darkTheme());
+// }
