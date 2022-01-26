30d1aca03c53fe315095974ea2af9a252097148c
Mugisha Precieux <precieuxmugisha@gmail.com>
2022-01-26 18:51:10 +0200

    daily push

--- js/buy.js
+++ js/buy.js
@@ -14,18 +14,3 @@ function darkTheme(){
         document.querySelector("#navbar").style.background = "#3d5af1"
     }
     
-// const nav = document.querySelector('#navbar')
-// window.addEventListener('scroll', fixNav)
-
-// // function fixNav() {
-// //     if(window.scrollY > nav.offsetHeight + 150) {
-// //         nav.classList.add('active')
-// //     } else {
-// //         nav.classList.remove('active')
-// //     }
-// // }
-// var icon = document.getElementById("modeChanger");
-
-// icon.onclick = function darkMode() {
-//     document.body.classList.toggle(darkTheme());
-// }
