71ccef1a97e5fed66dde44cd7a9970439a710cd0
Mugisha Precieux <precieuxmugisha@gmail.com>
2022-01-27 10:17:51 +0200

    added time trying to add advertisements and fixed the navbar

--- js/buy.js
+++ js/buy.js
@@ -13,4 +13,16 @@ function darkTheme(){
         document.body.style.backgroundColor = "white"
         document.querySelector("#navbar").style.background = "#3d5af1"
     }
-    
+    function displayTime(){
+const date = new Date()
+document.getElementById('time').innerHTML = date.toLocaleTimeString();
+setTimeout(function() {displayTime()},1000)
+    }
+    let image = document.getElementById('image')
+    let adDiv = document.getElementById('advert')
+    function advertisement(){
+setTimeout(function(){
+    image.style.display='block'
+    adDiv.appendChild(image)
+}, 1000)
+    }
