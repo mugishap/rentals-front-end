5347492b2ed306f3c25dddf422fa56fb1382ec34
Mugisha Precieux <precieuxmugisha@gmail.com>
2022-01-29 15:20:37 +0200

    working on new features

---
+++ ad.js
@@ -0,0 +1,14 @@
+function changeTheme(){
+  let body = document.querySelector('#body')
+  let btn = document.querySelector('#btn')
+  if(body.style.backgroundColor = 'white') {body.style.backgroundColor = 'black'}
+      else if(body.style.backgroundColor = 'black'){body.style.backgroundColor = 'white'}
+      body.addEventListener('')
+
+}
+let greeting = 'Hello Precieux, Welcome to Real Estate Housing'
+let holder = []
+for(i = 0 ; i < greeting.length ; i++){
+setTimeout(()=>{holder.push(greeting[i])}, 400)
+}
+document.querySelector('#para').innerHTML = holder
--- js/buy.js
+++ js/buy.js
@@ -1,28 +1,28 @@
-
-function darkTheme(){
+function darkTheme() {
     document.body.style.backgroundColor = "black"
     document.querySelector("#navbar").style.background = "darkblue"
-    a
+    document.querySelector("#footer").style.background = "darkblue"
+    
     document.querySelector("#goToDiv").style.cssText = ` = 
     background:darkblue;
     color:darkblue;
     `
 
-    }
-    function lightTheme(){
-        document.body.style.backgroundColor = "white"
-        document.querySelector("#navbar").style.background = "#3d5af1"
-    }
-    function displayTime(){
-const date = new Date()
-document.getElementById('time').innerHTML = date.toLocaleTimeString();
-setTimeout(function() {displayTime()},1000)
-    }
-    let image = document.getElementById('image')
-    let adDiv = document.getElementById('advert')
-    function advertisement(){
-setTimeout(function(){
-    image.style.display='block'
-    adDiv.appendChild(image)
-}, 1000)
-    }
+}
+function lightTheme() {
+    document.body.style.backgroundColor = "white"
+    document.querySelector("#navbar").style.background = "#3d5af1"
+}
+function displayTime() {
+    const date = new Date()
+    document.getElementById('time').innerHTML = date.toLocaleTimeString();
+    setTimeout(function () { displayTime() }, 1000)
+}
+let image = document.getElementById('image')
+let adDiv = document.getElementById('advert')
+function advertisement() {
+    setTimeout(function () {
+        image.style.display = 'block'
+        adDiv.appendChild(image)
+    }, 1000)
+}
