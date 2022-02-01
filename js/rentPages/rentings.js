08850d36b1dbc88bab74eaa8dca8c338af24b986
Mugisha Precieux <precieuxmugisha@gmail.com>
2022-02-01 14:43:50 +0200

    added interior images but not linked

--- js/rentPages/rentings.js
+++ js/rentPages/rentings.js
@@ -1,1 +1,26 @@
-function changeImage()
+function darkTheme() {
+    document.body.style.backgroundColor = "black"
+    document.querySelector("#navbar").style.cssText = ` = 
+    background: darkblue;
+    color: white;
+    `
+    document.querySelector("#goToDiv").style.cssText = ` = 
+    background:darkblue;
+    color:darkblue;
+    `
+
+}
+function lightTheme() {
+    document.body.style.backgroundColor = "white"
+    document.querySelector("#navbar").style.background = "#3d5af1"
+}
+function displayTime() {
+    const date = new Date()
+    document.getElementById('time').innerHTML = date.toLocaleTimeString();
+    setTimeout(function () { displayTime() }, 1000)
+}
+
+function showMap(){
+   let map = document.getElementById('map')
+map.style.display='flex'
+}
